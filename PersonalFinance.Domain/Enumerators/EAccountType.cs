da96aa7acbb49a7da4b5d8032feb20503b2e2592
André de Souza Ananias Garcia <anddregarcia@gmail.com>
2012-08-29 21:14:31 -0300

    enumerator EAccountType created
    enumerator EAccountType created

---
+++ PersonalFinance.Domain/Enumerators/EAccountType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonalFinance.Domain.Enumerators
+{
+    public enum EAccountType
+    {
+        BankAccount = 1,
+        CreditCard = 2,
+        PayOrReceive = 3,
+        Cash = 4,
+        Investiments = 5
+    }
+}
